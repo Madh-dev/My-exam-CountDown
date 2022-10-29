c870d65188c61a9fd106022ee332f333be0e5c8f
abdul <aleemabdulmuheez@gmail.com>
2022-10-29 15:37:14 +0100

    adding to the footer page

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='foot'>
-        <strong>
+        <b>
             While you are reading, don't forget to pray
-        </strong>
+        </b>
         <p>May God helps you in your reading and makes this a successful examination</p>
-
+        <p> You 're so close to the victory, don't you dare give up now &#9994;&#127998 &#9994;&#127998 <strong>DONT</strong></p>
 
     </div>
   )
