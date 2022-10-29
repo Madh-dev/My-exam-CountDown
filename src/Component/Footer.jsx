cbc896794863ca869214833c77eb9cc77189ffff
abdul <aleemabdulmuheez@gmail.com>
2022-10-29 15:48:25 +0100

    cool

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             While you are reading, don't forget to pray
         </b>
         <p>May God helps you in your reading and makes this a successful examination</p>
-        <p> You 're so close to the victory, don't you dare give up now <span>&#9994;&#127999 &#9994; </span><strong>DONT</strong></p>
+        <p> You 're so close to the victory, don't you dare give up now <span>&#9994;&#127999; &#9994;&#127999;</span><strong>DONT</strong></p>
 
     </div>
   )
